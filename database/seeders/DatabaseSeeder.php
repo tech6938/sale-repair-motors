d59aa563aa947122e1c7867639b6e27fa9eb0847
Taimoor Ali <saytaimoor@gmail.com>
2025-07-06 14:02:51 +0500

    Disable vehicle seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            VehicleSeeder::class,
+            // VehicleSeeder::class,
             InspectionTypeSeeder::class,
             InspectionChecklistSeeder::class,
         ]);
