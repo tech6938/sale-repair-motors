21b391dec45c7b7ccfa11f166fcf4a30300d9088
Mirza Arqam <mirzaarqam88@gmail.com>
2025-07-23 13:49:29 +0500

    datat

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -58,7 +58,7 @@
                     <li class="nk-menu-item">
                         <a href="{{ route('vehicles.index') }}" class="nk-menu-link">
                             <span class="nk-menu-icon"><em class="icon ni ni-truck"></em></span>
-                            <span class="nk-menu-text">Vehiclesss</span>
+                            <span class="nk-menu-text">Vehicless</span>
                         </a>
                     </li>
                 </ul>
